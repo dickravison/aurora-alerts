b984b3455d461f1a76df73daa5f8ecd4b2d25db1
Rick <rick@dvsn.io>
2024-12-23 14:36:26 +0000

    Refactoring after realising the API is returning the previous 24hrs rather than the next 24hrs

--- src/aurora_alerts/aurora_alerts.py
+++ src/aurora_alerts/aurora_alerts.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import pandas as pd
-import datetime
+from datetime import datetime
 import bs4
 import json
 import boto3
@@ -28,7 +28,7 @@ ALERT_ON_HIGH_ACTIVITY = False
 ALERT_ON_BAD_CONDITIONS = False
 ALERT_ON_PRE_SUNSET = False
 
-#THRESHOLDS
+# #THRESHOLDS
 AURORA_ACTIVITY_THRESHOLD = int(os.environ['AURORA_ACTIVITY_THRESHOLD'])
 CLOUDS_THRESHOLD = int(os.environ['CLOUD_THRESHOLD'])
 
@@ -47,7 +47,7 @@ def get_visibility():
     df = pd.DataFrame(data['hourly'])
 
     sunset = ''.join(data['daily']['sunset'])
-    sunset = datetime.datetime.strptime(sunset, '%Y-%m-%dT%H:%M').strftime('%H:%M')
+    sunset = datetime.strptime(sunset, '%Y-%m-%dT%H:%M').strftime('%H:%M')
     return df, sunset
 
 def main(event, context):
@@ -59,38 +59,26 @@ def main(event, context):
     activity_xml = bs4.BeautifulSoup(response.text, "xml")
     activity_bs = activity_xml.find_all('activity')
 
-    #Set empty arrays
-    high_activity = []
-    pre_sunset = []
-    bad_conditions = []
-    aurora_likely = []
+    now = datetime.today()
+    prev_hour = datetime.strftime(now.replace(second=0, microsecond=0, minute=0), '%Y-%m-%dT%H:%M:%S%z')
+    message = ""
 
     #Loop over each activity and add any times that have aurora activity higher than the threshold to the relevant arrays dependent on the visibility conditions.
     for activity in activity_bs:
         period = activity.find('datetime').get_text()
-        value = activity.find('value').get_text()
-        period = datetime.datetime.strptime(period, '%Y-%m-%dT%H:%M:%S%z')
-        period = period.strftime('%H:%M')
-        cloud_cover = clouds_df[clouds_df['time'].str.contains(period)]['cloud_cover'].values[0]
-        high_activity.append(period)
-        if float(value) > AURORA_ACTIVITY_THRESHOLD and sunset < period and int(cloud_cover) < CLOUDS_THRESHOLD:
-            aurora_likely.append(period)
-        elif float(value) > AURORA_ACTIVITY_THRESHOLD and sunset > period:
-            pre_sunset.append(period)
-        elif float(value) > AURORA_ACTIVITY_THRESHOLD and int(cloud_cover) > CLOUDS_THRESHOLD:
-            bad_conditions.append(period)
-
-    #Generate alert message
-    message = ""
-    if len(aurora_likely) > 0:
-        message = message + "The chance of seeing the aurora is high at the following times: " + ','.join(aurora_likely) + "\n"
-    if len(high_activity) > 0 and ALERT_ON_HIGH_ACTIVITY:
-        message = message + "There is high aurora activity during the following times: " + ','.join(high_activity) + "\n"
-    if len(pre_sunset) > 0 and ALERT_ON_PRE_SUNSET:
-        message = message + f"Sunset today is at {sunset}. It will be unlikely to see the aurora at these times: " + ','.join(high_activity) + "\n"
-    if len(bad_conditions) > 0 and ALERT_ON_BAD_CONDITIONS:
-        message = message + "It is quite cloudy so the chances of seeing the aurora are low at these times: " + ','.join(bad_conditions)
-    
+        print(period)
+        if period > prev_hour:
+            value = activity.find('value').get_text()
+            period = datetime.strptime(period, '%Y-%m-%dT%H:%M:%S%z')
+            period = period.strftime('%H:%M')
+            cloud_cover = clouds_df[clouds_df['time'].str.contains(period)]['cloud_cover'].values[0]
+            if float(value) > AURORA_ACTIVITY_THRESHOLD and sunset < period and int(cloud_cover) < CLOUDS_THRESHOLD:
+                message = message + "The chance of seeing the aurora is high."
+            elif float(value) > AURORA_ACTIVITY_THRESHOLD and sunset > period and ALERT_ON_PRE_SUNSET:
+                message = message + "Aurora activity is high but the sun has not set yet."
+            elif float(value) > AURORA_ACTIVITY_THRESHOLD and int(cloud_cover) > CLOUDS_THRESHOLD and ALERT_ON_BAD_CONDITIONS:
+                message = message + "Aurora activity is high but it is quite cloudy so the chances of seeing the aurora are low"
+        
     if message == "":
         message = "No chance of seeing Aurora"
     elif message != "" and NOTIFICATIONS_ENABLED == "true":
